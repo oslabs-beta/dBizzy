3f4b919ea825c0361f530d7a20ad5581eb149290
Johnny <johncschen@gmail.com>
2021-09-21 16:34:57 -0700

    fixed PK and FK parsing for sql visualizer

--- src/sample.sql
+++ src/sample.sql
@@ -22,7 +22,8 @@ city_ID int,
 city_name varchar(255),
 country_ID int,
 population int,
-PRIMARY KEY (city_ID)
+PRIMARY KEY (city_ID),
+FOREIGN KEY (city_ID) REFERENCES Countries(population),
 FOREIGN KEY (country_ID) REFERENCES Countries(country_ID)
 );
 
